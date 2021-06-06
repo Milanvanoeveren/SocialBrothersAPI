a7b06c9b6aed473c1b32c2f25fea62082f8c5a20
Milan <m.vanoeveren1@student.avans.nl>
2021-06-06 18:12:05 +0200

    removed weatherforecast classes + updated migrations

---
+++ SocialBrothers/TodoApi/Migrations/20210606160656_InitialCreate.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TodoApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Address",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Street = table.Column<string>(type: "TEXT", nullable: false),
+                    HouseNumber = table.Column<int>(type: "INTEGER", nullable: false),
+                    PostalCode = table.Column<string>(type: "TEXT", nullable: false),
+                    City = table.Column<string>(type: "TEXT", nullable: false),
+                    Country = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Address", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Address",
+                columns: new[] { "Id", "City", "Country", "HouseNumber", "PostalCode", "Street" },
+                values: new object[] { 1L, "Utrecht", "Netherlands", 35, "3446BG", "Berlengakade" });
+
+            migrationBuilder.InsertData(
+                table: "Address",
+                columns: new[] { "Id", "City", "Country", "HouseNumber", "PostalCode", "Street" },
+                values: new object[] { 2L, "Schagen", "Netherlands", 189, "1742EC", "Sperwerhof" });
+
+            migrationBuilder.InsertData(
+                table: "Address",
+                columns: new[] { "Id", "City", "Country", "HouseNumber", "PostalCode", "Street" },
+                values: new object[] { 3L, "Zoetermeer", "Netherlands", 92, "2711AB", "Promenadeplein" });
+
+            migrationBuilder.InsertData(
+                table: "Address",
+                columns: new[] { "Id", "City", "Country", "HouseNumber", "PostalCode", "Street" },
+                values: new object[] { 4L, "Amsterdam", "Netherlands", 82, "1021VG", "Kievitstraat" });
+
+            migrationBuilder.InsertData(
+                table: "Address",
+                columns: new[] { "Id", "City", "Country", "HouseNumber", "PostalCode", "Street" },
+                values: new object[] { 5L, "Maastricht", "Netherlands", 174, "6228XV", "Bronckweg" });
+
+            migrationBuilder.InsertData(
+                table: "Address",
+                columns: new[] { "Id", "City", "Country", "HouseNumber", "PostalCode", "Street" },
+                values: new object[] { 6L, "Breda", "Netherlands", 134, "4827AG", "Willem van Boelrestraat" });
+
+            migrationBuilder.InsertData(
+                table: "Address",
+                columns: new[] { "Id", "City", "Country", "HouseNumber", "PostalCode", "Street" },
+                values: new object[] { 7L, "Enkhuizen", "Netherlands", 133, "1601JS", "Apenspel" });
+
+            migrationBuilder.InsertData(
+                table: "Address",
+                columns: new[] { "Id", "City", "Country", "HouseNumber", "PostalCode", "Street" },
+                values: new object[] { 8L, "Waterlandkerkje", "Netherlands", 106, "4508PH", "Plaatweg" });
+
+            migrationBuilder.InsertData(
+                table: "Address",
+                columns: new[] { "Id", "City", "Country", "HouseNumber", "PostalCode", "Street" },
+                values: new object[] { 9L, "Oosterbeek", "Netherlands", 119, "6861XZ", "Noorderweg" });
+
+            migrationBuilder.InsertData(
+                table: "Address",
+                columns: new[] { "Id", "City", "Country", "HouseNumber", "PostalCode", "Street" },
+                values: new object[] { 10L, "Meppel", "Netherlands", 17, "7941CH", "Bloemendalstraat" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Address");
+        }
+    }
+}
--- SocialBrothers/TodoApi/Models/DatabaseContext.cs
+++ SocialBrothers/TodoApi/Models/DatabaseContext.cs
@@ -18,10 +18,16 @@ namespace TodoApi.Models
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Address>().HasData(
-            new Address() { Id = 1, HouseNumber = 1, Country = "Netherlands", City = "Utrecht", Street = "Berlengakade", PostalCode = "3446BG" },
-            new Address() { Id = 2, HouseNumber = 2, Country = "Netherlands", City = "Groningen", Street = "Straatweg", PostalCode = "5461AB" },
-            new Address() { Id = 3, HouseNumber = 3, Country = "Netherlands", City = "Rotterdam", Street = "Lange Weg", PostalCode = "5689FW" },
-            new Address() { Id = 4, HouseNumber = 4, Country = "Netherlands", City = "Amsterdam", Street = "Linschoterweg", PostalCode = "1285GB" }
+            new Address() { Id = 1, Street = "Berlengakade", HouseNumber = 35, PostalCode = "3446BG", City = "Utrecht", Country = "Netherlands" },
+            new Address() { Id = 2, Street = "Sperwerhof", HouseNumber = 189, PostalCode = "1742EC", City = "Schagen", Country = "Netherlands" },
+            new Address() { Id = 3, Street = "Promenadeplein", HouseNumber = 92, PostalCode = "2711AB", City = "Zoetermeer", Country = "Netherlands" },
+            new Address() { Id = 4, Street = "Kievitstraat", HouseNumber = 82, PostalCode = "1021VG", City = "Amsterdam", Country = "Netherlands" },
+            new Address() { Id = 5, Street = "Bronckweg", HouseNumber = 174, PostalCode = "6228XV", City = "Maastricht", Country = "Netherlands" },
+            new Address() { Id = 6, Street = "Willem van Boelrestraat", HouseNumber = 134, PostalCode = "4827AG", City = "Breda", Country = "Netherlands" },
+            new Address() { Id = 7, Street = "Apenspel", HouseNumber = 133, PostalCode = "1601JS", City = "Enkhuizen", Country = "Netherlands" },
+            new Address() { Id = 8, Street = "Plaatweg", HouseNumber = 106, PostalCode = "4508PH", City = "Waterlandkerkje", Country = "Netherlands" },
+            new Address() { Id = 9, Street = "Noorderweg", HouseNumber = 119, PostalCode = "6861XZ", City = "Oosterbeek", Country = "Netherlands" },
+            new Address() { Id = 10, Street = "Bloemendalstraat", HouseNumber = 17, PostalCode = "7941CH", City = "Meppel", Country = "Netherlands" }
             );
         }
     }
